66b2182c8b06a5719d20eae5dbef3240a7902c4f
Tsukatsuki Kanade <i@sukitsuki.com>
2018-12-04 19:25:18 +0700

    Fix stylus

---
+++ src/css/hexo-config.js
@@ -0,0 +1,43 @@
+const YAML = require('yaml');
+const fs = require('fs');
+
+const file = fs.readFileSync('./config.yml', 'utf8');
+const _file = fs.readFileSync('./_config.yml', 'utf8');
+
+const _config = YAML.parse(_file);
+const config = Object.assign(_config, YAML.parse(file));
+
+function getProperty(obj, name) {
+  name = name.replace(/\[(\w+)]/g, '.$1').replace(/^\./, '');
+
+  const split = name.split('.');
+  let key = split.shift();
+
+  if (!obj.hasOwnProperty(key)) return '';
+
+  let result = obj[key];
+  let len = split.length;
+
+  if (!len) return result || '';
+  if (typeof result !== 'object') return '';
+
+  for (let i = 0; i < len; i++) {
+    key = split[i];
+    if (!result.hasOwnProperty(key)) return '';
+
+    result = result[split[i]];
+    if (typeof result !== 'object') return result;
+  }
+
+  return result;
+}
+
+const plugin = function () {
+  return function (style) {
+    style.define('hexo-config', function (data) {
+      return getProperty(config, data.val);
+    });
+  };
+};
+
+module.exports = plugin;
