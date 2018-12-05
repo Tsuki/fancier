81a249decfcb58b93ede0c6090972961d6f18a21
Tsukatsuki Kanade <i@sukitsuki.com>
2018-12-05 22:28:03 +0800

    Fix space and animation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,7 +2,7 @@ import {AfterViewInit, Component, HostBinding, OnInit} from '@angular/core';
 import {ApiService} from "~/service/api.service";
 import {HexoConfig, ThemeConfig} from "~/model/hexo-config.class";
 import {tap} from "rxjs/operators";
-import {animate, query, stagger, state, style, transition, trigger} from "@angular/animations";
+import {animate, group, query, stagger, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-header',
@@ -11,13 +11,18 @@ import {animate, query, stagger, state, style, transition, trigger} from "@angul
   animations: [
     trigger('header', [
       // state("0", style({opacity: 0, transform: 'translateY(-15%)'})),
-      transition(':enter', [
-        query('*', style({opacity: 0, transform: 'translateY(-15%)'}), { optional: true }),
-        // query('.site-title', style({transform: 'translateY(-15%)'})),
-        query('span.site-title', animate('2s', style({opacity: 1, transform: 'translateY(0)'})), {optional: true}),
-        query('.menu-item', stagger('2s', [
-          animate('.3s ease-in', style({opacity: 1, transform: 'translateY(0)'}))
-        ]), { optional: true })
+      transition('* => 1', [
+        query('.site-title', style({opacity: 0, transform: 'translateY(-30%)'})),
+        query('.menu-item', style({opacity: 0})),
+        query('.logo-line-before > i', style({transform: 'translateX(-100%)'})),
+        query('.logo-line-after > i', style({transform: 'translateX(100%)'})),
+        group([
+          query('.logo-line-before > i', animate('500ms', style({transform: 'translateX(0)'}))),
+          query('.logo-line-after > i', animate('500ms', style({transform: 'translateX(0)'}))),
+        ]),
+        query('.site-title', animate('500ms', style({opacity: 1, transform: 'translateY(0)'}))),
+        query('.menu-item', stagger('200ms', [
+          animate('.3s ease-in', style({opacity: 1, transform: 'translateY(0)'}))]))
       ])
     ])
   ]
