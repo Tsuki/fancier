e91e951021b3149dbf374acee5d430cc98e66880
Tsukatsuki Kanade <i@sukitsuki.com>
2018-12-12 15:19:09 +0700

    animation

---
+++ src/app/utils/animation.ts
@@ -0,0 +1,8 @@
+import {animate, query, style, transition, trigger} from "@angular/animations";
+
+export const postAnimation = trigger('post', [
+  transition('* => 1', [
+    query('#posts', style({opacity: 0, transform: 'translateY(-50px)'}),),
+    query('#posts', animate('1s 1s ease-in', style({opacity: 1, transform: 'translateY(0)'})),),
+  ])
+]);
--- src/app/views/page/page.component.ts
+++ src/app/views/page/page.component.ts
@@ -1,15 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostBinding, OnInit} from '@angular/core';
 import {HexoConfig, Theme_config} from '~/model/site-config.class';
 import {ApiService} from '~/service/api.service';
 import {switchMap, tap} from 'rxjs/operators';
 import {PostsList} from '~/model/posts-list.class';
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {ObservableService} from "~/service/observable.service";
+import {postAnimation} from "~/utils/animation";
+import {AnimationService} from "~/service/animation.service";
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.styl']
+  styleUrls: ['./page.component.styl'],
+  animations: [postAnimation]
 })
 export class PageComponent implements OnInit {
   hexoConfig: HexoConfig;
@@ -18,6 +21,7 @@ export class PageComponent implements OnInit {
   isIndex = false;
   currentPage: number = 1;
   mid_size = 1;
+  @HostBinding('@post') useMotion = false;
 
   constructor(
     private api: ApiService,
@@ -47,7 +51,8 @@ export class PageComponent implements OnInit {
   initPageJson() {
     this.api.fetchPostsList(this.currentPage).pipe(
       tap(value => this.postsList = value)
-    ).subscribe()
+    ).subscribe(() => this.useMotion = true)
   }
+
 
 }
