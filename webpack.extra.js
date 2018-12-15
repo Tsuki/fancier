0c275978f645a771c58347e4b2847e1be9eb16d6
Tsukatsuki Kanade <i@sukitsuki.com>
2018-12-15 13:39:39 +0700

    "Add sync defer"

--- webpack.extra.js
+++ webpack.extra.js
@@ -1,6 +1,25 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
+const METADATA = {
+  baseUrl: '/',
+  // title: options.title,
+  // isDevServer: helpers.isWebpackDevServer(),
+  // HMR: HMR
+};
 module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      title: METADATA.title,
+      chunksSortMode: 'dependency',
+      metadata: METADATA,
+      inject: 'head'
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      async: 'polyfills',
+      defaultAttribute: 'defer'
+    })
   ]
 };
