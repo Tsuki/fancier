7bdea8a1d826051f946a2c6ed6501f9e277c018c
Tsukatsuki Kanade <i@sukitsuki.com>
2018-12-15 11:29:12 +0700

    Add sync

--- webpack.extra.js
+++ webpack.extra.js
@@ -1,6 +1,12 @@
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    new HtmlWebpackPlugin(),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'async'
+    })
   ]
 };
